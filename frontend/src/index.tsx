ca8b3856d6d00cc7586612c66399fb9876648e9d
Bahram <baxxxrrram@yandex.ru>
2022-11-09 16:57:53 +0400

    initial files frontend

---
+++ frontend/src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
+
+import './index.css';
+import { theme } from './theme/theme';
+
+import App from './App';
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ frontend/src/theme/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+
+export const theme = createTheme({
+    palette: {
+      primary: {
+        main: red[500],
+      },
+    },
+  });
