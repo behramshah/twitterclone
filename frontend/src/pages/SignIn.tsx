545be1c5191b224b9394ca8340ef703781a13417
Bahram <baxxxrrram@yandex.ru>
2022-11-11 17:51:30 +0400

    frontend styling

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useTheme } from '@mui/material/styles';
 import { makeStyles } from 'tss-react/mui';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 export default function SignIn() {
 
@@ -17,8 +18,19 @@ export default function SignIn() {
       flex: '0 0 50%',
     },
     loginSide: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
       flex: '0 0 50%',
     },
+    button: {
+      fontSize: '16px',
+      'border-radius': '25px',
+    },
+    loginSideTwitterIcon: {
+      fontSize: '45px',
+    },
+  
   }));
   
 
@@ -32,14 +44,21 @@ export default function SignIn() {
             <Typography>Read intersting things</Typography>
           </li>
           <li>
-            <Typography>Learn news all around the world</Typography>
+            <Typography>Don’t miss what’s happening</Typography>
           </li>
           <li>
-            <Typography>Communicate with friends</Typography>
+            <Typography>People on Twitter are the first to know.</Typography>
           </li>
         </ul>
       </section>
       <section className={classes.loginSide}>
+        <div>
+          <TwitterIcon className={classes.loginSideTwitterIcon} color='primary'/>
+          <Typography>New to Twitter?</Typography>
+          <Typography>Sign up now to get your own personalized timeline!</Typography>
+          <Button className={classes.button} variant='contained' color='primary' fullWidth >Sign up</Button>
+          <Button className={classes.button} variant='outlined' color='primary' fullWidth >Log in</Button>
+        </div>
       </section>
     </div>
   );
